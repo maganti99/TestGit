a096e7bd25bf1678d8b7a680d7fdcda901ff097a
MZGW <nagesh.maganti@perspecta.com>
2020-08-20 10:56:39 -0400

    Initial change

---
+++ src/main/java/com/maganti/Test1.java
@@ -0,0 +1,27 @@
+package com.maganti;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+		List<Integer> nums = new ArrayList<Integer>();
+		nums.add(5);
+		nums.add(9);
+		nums.add(10);
+		nums.add(20);		
+		
+		for(int i:nums)
+		{
+			sum = sum + i;
+		}
+		
+		System.out.println("Sum is : " + sum);
+		
+
+	}
+
+}
