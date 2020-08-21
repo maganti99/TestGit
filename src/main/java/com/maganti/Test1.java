85c4c62506f5ff50499484f74ef92745cbc321ea
MZGW <nagesh.maganti@perspecta.com>
2020-08-21 12:22:22 -0400

    changed summing from For loop to Stream/reduce

--- src/main/java/com/maganti/Test1.java
+++ src/main/java/com/maganti/Test1.java
@@ -2,11 +2,12 @@ package com.maganti;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		int sum=0;
+		Optional<Integer> sum;
 		
 		List<Integer> nums = new ArrayList<Integer>();
 		nums.add(5);
@@ -15,10 +16,8 @@ public class Test1 {
 		nums.add(20);
 		nums.add(30);
 		
-		for(int i:nums)
-		{
-			sum = sum + i;
-		}
+		sum = nums.stream().reduce((s1,s2) -> s1+s2);
+		
 		
 		System.out.println("Sum is : " + sum);
 		
